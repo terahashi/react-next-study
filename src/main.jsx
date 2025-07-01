e22ef1d59f03b3543b66fe2dd332137fba33eb02
terahashi <tkshplant@outlook.jp>
2025-07-01 19:27:12 +0900

    reactの読み込み順番。main.jsx➡︎App.jsx➡︎pages/Home.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+//1:多くのプロジェクトではmain.jsxがスタートです。
+//2:次にApp.jsxを読み込む。App.jsx は、全ページの親（ルート）です。
+//3:「pagesのHome.jsx(トップページを読み込む)」設定を追加します。
+
+///まとめ:ブラウザで http://localhost:3000/ にアクセスしたとき
+//1:まず main.jsx → App.jsx が読み込まれる
+//2:App.jsx の中で <Routes> が URL に応じて <Home /> をレンダリングする
+//3:だから結果的に画面に出るのは pages/Home.jsx の内容が表示される。
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
