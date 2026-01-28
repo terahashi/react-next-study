96a85258e85dedce120dae1876b242fabf9ae2a5
terahashi <tkshplant@outlook.jp>
2026-01-28 17:43:00 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -39,6 +39,9 @@ function App() {
         <a href='https://react.dev' target='_blank'>
           <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
+         <a href='https://react.dev' target='_blank'>
+          test
+        </a>
       </div> */}
 
       {/* <h1>Vite + React</h1> */}
